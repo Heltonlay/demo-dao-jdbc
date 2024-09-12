db3ee99b6df8eeb6120428c8377ca6e64531909f
heltonlay <heltonlay15@gmail.com>
2024-09-11 23:11:13 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Department dp = new Department(1, "Books");
+		System.out.println(dp);
 	}
 
 }
