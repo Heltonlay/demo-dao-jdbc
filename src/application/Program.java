62027c5b5c71cb6d9c165e8ebb2d509d62d76c04
Heltonlay <heltonlay15@gmail.com>
2024-09-13 09:03:26 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,14 +10,18 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("TESTE 1");
+		System.out.println("TESTE 1 - findById");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
 
-		System.out.println("\nTESTE 2");
+		System.out.println("\nTESTE 2 - findByDepartment");
 		List<Seller> sellers = sellerDao.findByDepartment(new Department(2, null));
 		sellers.stream().forEach(System.out::println);
+		
+		System.out.println("\nTESTE 3 - findAll");
+		List<Seller> allSellers = sellerDao.findAll();
+		allSellers.stream().forEach(System.out::println);
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -79,7 +79,32 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+			rs = st.executeQuery();
+			
+			List<Seller> sellers = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(dep.getId(), dep);
+				}
+				Seller seller = instantiateSeller(rs, dep);
+				sellers.add(seller);
+			}
+			return sellers;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 	
 	@Override
@@ -99,7 +124,7 @@ public class SellerDaoJDBC implements SellerDao {
 			Map<Integer, Department> map = new HashMap<>();
 			
 			while (rs.next()) {
-				Department dep = map.get(department.getId());
+				Department dep = map.get(rs.getInt("DepartmentId"));
 				if(dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(dep.getId(), dep);
